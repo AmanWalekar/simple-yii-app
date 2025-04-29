0773bd28e3223ef00a8d32d6068e2f5ad32ae368
amanwalekar <amanwalekar11@gmail.com>
2025-04-29 14:49:06 +0530

    initial commit

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'yii2',
+    'password' => 'LUfdVw7Jw5scvFbB',
     'charset' => 'utf8',
 ]; 
